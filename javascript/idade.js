31326441f42c27d3be8bb7bdb14551398edc39de
rich-01-hash <richard_pinheiro@estudante.sesisenai.org.br>
2025-05-27 17:08:04 -0300

    1- exercicio javascript

---
+++ javascript/idade.js
@@ -0,0 +1,11 @@
+//ler a idade do usúario
+
+//se a iade 18 ou mais,dizer que é maior
+//senão, diz que é maior
+
+let idade= prompt("qual sua idade?")
+  if(idade >= 18){
+    alert("você é maior de idade")
+}else{
+    alert("você é menor de idade")
+}
